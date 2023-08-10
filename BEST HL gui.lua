767cd62c83ea68ae9d6baad62628636019093675
sharkifyy <130886630+sharkifyy@users.noreply.github.com>
2023-08-10 15:42:33 -0500

    Update BEST HL gui.lua

--- BEST HL gui.lua
+++ BEST HL gui.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sharkifyy/sharkifyV3/main/2.3", true))()
+-- hood legends gui just has the basics and has a AC destroyer
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sharkifyy/sharkifyHLobfuscated/main/loadstring.lua", true))()
