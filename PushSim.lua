9e719f37a27e838cb205a280eb89e2408dca1a59
sharkifyy <130886630+sharkifyy@users.noreply.github.com>
2023-08-11 17:23:46 -0500

    Update PushSim.lua

--- PushSim.lua
+++ PushSim.lua
@@ -12,7 +12,7 @@
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
-local Window = Library.CreateLib("push sim", "BloodTheme")
+local Window = Library.CreateLib("push sim", "Ocean")
 
 local Tab = Window:NewTab("main")
 
@@ -22,6 +22,10 @@ local Section = Tab:NewSection("boosts")
 game:GetService("ProximityPromptService").PromptButtonHoldBegan:Connect(function(prompt) -- is auto executed.
     prompt.HoldDuration = 0
 end)
+
+
+game:GetService("Players").LocalPlayer.PlayerGui.UI.Notification:Destroy() -- auto executed 
+
 
 
 Section:NewButton("power gain", "ButtonInfo", function()
@@ -46,6 +50,11 @@ game:GetService("ReplicatedStorage").Remotes.PowerGain:FireServer()
 
  Wait()
 end
+end)
+
+
+Section:NewSlider("WalkSpeed changer!", "SliderInfo", 500, 0, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
 
@@ -92,5 +101,45 @@ while _G.testt == true do
 
  Wait()
 end
+end)
+
+
+
+
+----------------------------------TP'S----------------------------------
+
+
+Section:NewLabel("TP'S") -- sorry that i couldnt make it another Section.
+
+Section:NewButton("1", "ButtonInfo", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-80.6999969, 9.89999962, -19.9500008, 0, 0, -1, 0, 1, 0, 1, 0, 0) -- main world
+end)
+
+Section:NewButton("2", "ButtonInfo", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(631.853638, 809.900024, -122.222565, 0, 0, 1, 0, 1, -0, -1, 0, 0) -- beach world
+end)
+
+Section:NewButton("3", "ButtonInfo", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1432.75366, 1609.90002, -122.422562, 0, 0, 1, 0, 1, -0, -1, 0, 0)  -- lava
+end)
+
+Section:NewButton("4", "ButtonInfo", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2107.03394, 2109.8999, -12.5375671, 0, 0, 1, 0, 1, -0, -1, 0, 0)  -- ancient world
+end)
+
+Section:NewButton("5", "ButtonInfo", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3517.93408, 2559.8999, -12.5375671, 0, 0, 1, 0, 1, -0, -1, 0, 0)  -- icy 
+end)
+
+Section:NewButton("6", "ButtonInfo", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5007.93408, 3749.8999, -12.5375671, 0, 0, 1, 0, 1, -0, -1, 0, 0)  -- space
+end)
+
+Section:NewButton("7", "ButtonInfo", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(6707.93408, 5949.8999, -12.5375671, 0, 0, 1, 0, 1, -0, -1, 0, 0)  -- ATLANTIS?""
+end)
+
+Section:NewButton("8", "ButtonInfo", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new() -- ????
 end)
 
