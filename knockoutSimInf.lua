6cd7f85dbfbb78bab7fe426df66df35a24838b89
sharkifyy <130886630+sharkifyy@users.noreply.github.com>
2023-08-12 22:19:28 -0500

    Create knockoutSimInf.lua

---
+++ knockoutSimInf.lua
@@ -0,0 +1,4 @@
+-- this is just inf strength in knockout sim im making a GUI for this game. also very easy script ik
+while wait() do
+game:GetService("ReplicatedStorage").Event.Train:FireServer(99999999999999)
+end
